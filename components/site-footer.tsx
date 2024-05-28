e6ed377d5d278aae6c2692c918e72eb62cbe4df3
johndeniel <johndeniel.d37@gmail.com>
2024-05-28 19:02:18 +0800

    feat: Add GitHub button to footer

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,5 +1,8 @@
 import Link from 'next/link'
 import { Shell } from '@/components/ui/shell'
+import { GitHubIcon } from '@/lib/icons'
+import { buttonVariants } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
 
 export function SiteFooter() {
   return (
@@ -15,6 +18,23 @@ export function SiteFooter() {
               className="font-semibold transition-colors hover:text-foreground"
             >
               jaydeeclouds
+            </Link>
+          </div>
+          
+          <div className="flex items-center space-x-1">
+            <Link
+              href="https://github.com/johndeniel"
+              target="_blank"
+              rel="noreferrer"
+              className={cn(
+                buttonVariants({
+                  size: 'icon',
+                  variant: 'ghost',
+                })
+              )}
+            >
+              <GitHubIcon className="size-5" aria-hidden="true" />
+              <span className="sr-only">GitHub</span>
             </Link>
           </div>
         </section>
