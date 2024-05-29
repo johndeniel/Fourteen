6238c21064c6ae6f03fc8ad36b4b8b1dfce047e8
johndeniel <johndeniel.d37@gmail.com>
2024-05-29 15:12:46 +0800

    feat: Initialize Firebase and set up Realtime Database

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
+'use client'
+
 import { HeroSection } from '@/components/hero-section'
 import { CardSnapshot } from '@/components/card-snapshot'
 import { SiteFooter } from '@/components/site-footer'
+import { useEffect, useState } from 'react'
+import { ref, get } from 'firebase/database'
+import database from '@/lib/firebase-config'
 
 export default function Home() {
+  const [data, setData] = useState(null)
+  useEffect(() => {
+    const fetchData = async () => {
+      const coverRef = ref(database, 'cover')
+      const snapshot = await get(coverRef)
+      if (snapshot.exists()) {
+        setData(snapshot.val())
+        console.log(snapshot.val())
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
     <main className="relative h-full w-full items-center  justify-center bg-white bg-dot-black/[0.2] dark:bg-black dark:bg-dot-white/[0.2]">
       {/* Radial gradient for the container to give a faded look */}
