433a3d3b61a8467ed59fc6af688aec3c5cbf8f7a
johndeniel <johndeniel.d37@gmail.com>
2024-05-29 08:12:09 +0800

    chore: Configure ESLint and Prettier to use CRLF and simplify Tailwind styles for SiteFooter

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -6,39 +6,35 @@ import { cn } from '@/lib/utils'
 
 export function SiteFooter() {
   return (
-    <footer className="w-full">
-      <Shell>
-        <section className="z-10 flex items-center space-x-4">
-          <div className="flex-1 text-left text-lg leading-loose">
-            Follow{' '}
-            <Link
-              href="https://www.instagram.com/jaydeeclouds"
-              target="_blank"
-              rel="noreferrer"
-              className="font-semibold transition-colors hover:text-foreground"
-            >
-              jaydeeclouds
-            </Link>
-          </div>
-
-          <div className="flex items-center space-x-1">
-            <Link
-              href="https://github.com/johndeniel"
-              target="_blank"
-              rel="noreferrer"
-              className={cn(
-                buttonVariants({
-                  size: 'icon',
-                  variant: 'ghost',
-                }),
-              )}
-            >
-              <GitHubIcon className="size-5" aria-hidden="true" />
-              <span className="sr-only">GitHub</span>
-            </Link>
-          </div>
-        </section>
-      </Shell>
-    </footer>
+    <Shell className="w-full">
+      <section className="z-10 flex items-center">
+        <div className="flex-1">
+          Follow{' '}
+          <Link
+            href="https://www.instagram.com/jaydeeclouds"
+            target="_blank"
+            rel="noreferrer"
+            className="font-semibold"
+          >
+            jaydeeclouds
+          </Link>
+        </div>
+        <div>
+          <Link
+            href="https://github.com/johndeniel"
+            target="_blank"
+            rel="noreferrer"
+            className={cn(
+              buttonVariants({
+                size: 'icon',
+                variant: 'ghost',
+              }),
+            )}
+          >
+            <GitHubIcon className="size-5" />
+          </Link>
+        </div>
+      </section>
+    </Shell>
   )
 }
