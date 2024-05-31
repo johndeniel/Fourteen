4d3053a7cbf6fd19bb9bc84c03738ad71c8127a7
johndeniel <johndeniel.d37@gmail.com>
2024-05-31 21:32:37 +0800

    style: Add container div to prevent layout shift on zoom

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { Cover } from '@/lib/model/cover'
 
 export default function Home() {
   return (
-    <main className="relative h-full w-full items-center  justify-center bg-white bg-dot-black/[0.2] dark:bg-black dark:bg-dot-white/[0.2]">
+    <main className="relative h-full w-full items-center justify-center bg-white bg-dot-black/[0.2] sm:container dark:bg-black dark:bg-dot-white/[0.2]">
       {/* Radial gradient for the container to give a faded look */}
       <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
       <HeroSection />
