6eae4facef020e92d36f19f3b52af7370105143f
johndeniel <johndeniel.d37@gmail.com>
2024-07-17 10:18:33 +0800

    refactor: rearrange the folder structure

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,8 +1,10 @@
-interface ArticleLayoutProps {
+interface MarketingLayoutProps {
   children: React.ReactNode
 }
 
-export default async function ArticleLayout({ children }: ArticleLayoutProps) {
+export default async function MarketingLayout({
+  children,
+}: MarketingLayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
       <main className="flex-1">{children}</main>
